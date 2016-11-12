c807a6eee5a5fa5ac43a3f99afe109e4348af278
squirlemaster42 <squirlemaster@gmail.com>
2016-11-12 01:40:16 -0500

    Fixed exception

--- src/org/usfirst/frc/team1699/utils/autonomous/CommandNotFoundException.java
+++ src/org/usfirst/frc/team1699/utils/autonomous/CommandNotFoundException.java
@@ -2,10 +2,16 @@ package org.usfirst.frc.team1699.utils.autonomous;
 
 public class CommandNotFoundException extends RuntimeException {
 
-	/**
-	 * 
-	 */
+	// Auto-generated Serial ID
 	private static final long serialVersionUID = 3380041418989057044L;
+
+	// Auto-generated constructors, praise Eclipse
+	public CommandNotFoundException() {
+		super();
+	}
+
+	public CommandNotFoundException(String arg0) {
+		super(arg0);
+	}
 	
-	//Finish, add needed methods
 }
