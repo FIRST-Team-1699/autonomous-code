e4025eea216456386ec47351b879c382454dae9b
squirlemaster42 <squirlemaster@gmail.com>
2016-11-21 18:33:42 -0500

    Comments

--- src/org/usfirst/frc/team1699/utils/drive/DriveUtils.java
+++ src/org/usfirst/frc/team1699/utils/drive/DriveUtils.java
@@ -6,7 +6,7 @@ import edu.wpi.first.wpilibj.SpeedController;
 
 public class DriveUtils {
 	
-	public static void driveForTime(SpeedController motCont, double timeAmt, double speed){
+	public static void driveForTime(SpeedController motCont, double timeAmt, double speed){ //Used to tell a motor to run for a specified amount of time
 		BetterTimer time = new BetterTimer();
 		time.start();
 		do{
