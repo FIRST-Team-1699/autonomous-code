ee4638e1463c48d584981541cd4a80d3d75fb747
Jakob Misbach <jakobmisbach8@gmail.com>
2016-12-02 09:11:45 -0500

    Update AutoCommand.java

--- src/org/usfirst/frc/team1699/utils/autonomous/AutoCommand.java
+++ src/org/usfirst/frc/team1699/utils/autonomous/AutoCommand.java
@@ -1,3 +1,18 @@
 public interface AutoCommand{
-  
+  /**
+	 * This method should contain code run by auto script
+	 * 
+	 * @param distance
+	 * @param speed
+	 * @param useSensor
+	 */
+	public void runAuto(double distance, double speed, boolean useSensor); //Abstract method that should hold code that is supposed to be called during auto when the command is called
+	
+	/**
+	 * This method should contains code that returns if the auto command is done
+	 * 
+	 * @return
+	 */
+	public boolean autoCommandDone(); //Abstract method that should hold code that should return true if the auto command is done
+	
 }
