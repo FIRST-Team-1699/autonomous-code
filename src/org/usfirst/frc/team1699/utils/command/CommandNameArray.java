b50f7f648fb460c85097202d87373a46e2043f7d
squirlemaster42 <squirlemaster@gmail.com>
2016-12-06 16:32:59 -0500

    Now extends ArrayList, needs improvement.

--- src/org/usfirst/frc/team1699/utils/command/CommandNameArray.java
+++ src/org/usfirst/frc/team1699/utils/command/CommandNameArray.java
@@ -11,8 +11,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
-@SuppressWarnings("rawtypes")
-public class CommandNameArray implements List{
+public class CommandNameArray extends ArrayList{
 	
 	private ArrayList<String> list; //Hold list of names
 
@@ -36,7 +35,13 @@ public class CommandNameArray implements List{
 		list.add(name);
 	}
 	
-	public void setName(int index, String name){
+	/**
+	 * Puts a name at a certain index
+	 * 
+	 * @param index
+	 * @param name
+	 */
+	public void setName(int index, String name){ //Used to put a name as a specific index
 		if(list.contains(name)){
 			System.err.println("Id has already been used.");
 			throw new NameUsedException();
@@ -53,125 +58,4 @@ public class CommandNameArray implements List{
 		return list;
 	}
 	
-	@Override
-	public int size() {
-		return list.size();
-	}
-
-	@Override
-	public boolean isEmpty() {
-		return list.isEmpty();
-	}
-
-	@Override
-	public boolean contains(Object o) {
-		return list.contains(o);
-	}
-	
-	@Override
-	public Iterator iterator() {
-		return list.iterator();
-	}
-
-	@Override
-	public Object[] toArray() {
-		return list.toArray();
-	}
-
-	@Override
-	public Object[] toArray(Object[] a) {
-		return list.toArray(a);
-	}
-
-	@Override
-	@Deprecated
-	public boolean add(Object e) {
-		return list.add((String) e);
-	}
-
-	@Override
-	public boolean remove(Object o) {
-		return list.remove(o);
-	}
-
-	@Override
-	public boolean containsAll(Collection c) {
-		return list.containsAll(c);
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	@Deprecated
-	public boolean addAll(Collection c) {
-		return list.addAll(c);
-	}
-
-	@SuppressWarnings("unchecked")
-	@Override
-	@Deprecated
-	public boolean addAll(int index, Collection c) {
-		return list.addAll(index, c);
-	}
-
-	@Override
-	public boolean removeAll(Collection c) {
-		return list.removeAll(c);
-	}
-
-	@Override
-	public boolean retainAll(Collection c) {
-		return list.retainAll(c);
-	}
-
-	@Override
-	public void clear() {
-		list.clear();
-	}
-
-	@Override
-	public Object get(int index) {
-		return list.get(index);
-	}
-
-	@Override
-	public Object set(int index, Object element) {
-		return list.set(index, (String) element);
-	}
-
-	@Override
-	@Deprecated
-	public void add(int index, Object element) {
-		list.add(index, (String) element); 
-	}
-
-	@Override
-	public Object remove(int index) {
-		return list.remove(index);
-	}
-
-	@Override
-	public int indexOf(Object o) {
-		return list.indexOf(o);
-	}
-
-	@Override
-	public int lastIndexOf(Object o) {
-		return list.lastIndexOf(o);
-	}
-
-	@Override
-	public ListIterator listIterator() {
-		return list.listIterator();
-	}
-		
-	@Override
-	public ListIterator listIterator(int index) {
-		return list.listIterator(index);
-	}
-
-	@Override
-	public List subList(int fromIndex, int toIndex) {
-		return list.subList(fromIndex, toIndex);
-	}
-
 }
