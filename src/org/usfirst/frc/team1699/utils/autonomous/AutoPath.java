7c08d0437be2826a8f4e4990dbb550462946e462
squirlemaster42 <squirlemaster@gmail.com>
2016-12-17 00:51:48 -0500

    Fixed comments for single lines

--- src/org/usfirst/frc/team1699/utils/autonomous/AutoPath.java
+++ src/org/usfirst/frc/team1699/utils/autonomous/AutoPath.java
@@ -55,6 +55,7 @@ public class AutoPath {
 	public void runScript(){
 		for(int i = 0; i < fileAsString.size(); i++){ //Loops through fileAsString array
 			try{
+				System.out.println(fileAsString.get(i));
 				System.out.println(removeLineComments(fileAsString.get(i)));
 				callCommandFromString(fileAsString.get(i)); //sends string to method so it can be converted to an object
 			}catch(CommandNotFoundException e){ //Detects if there is a error where the command is not found
@@ -170,13 +171,14 @@ public class AutoPath {
 	 * @return newLine
 	 */
 	public static String removeLineComments(String line) {
-		String newLine = ""; //Makes a new version of the line without comments to be returned
+		String newLine = line; //Makes a new version of the line without comments to be returned
 		if(line.substring(0, 1).equals("~") || line.substring(0, 2).equals("//")) { //Returns nothing if the entire line is a single-line comment
-			return newLine;
+			return "";
 		}
 		for(int ch = 0; ch < line.length() - 2; ch++) { //Loops through the string and finds the start of any line comments to be omitted
 			if ((line.substring(ch, ch + 1).equals("~")) || (line.substring(ch, ch + 2).equals("//"))) {
 				newLine = line.substring(0, ch);
+				return newLine;
 			}
 		}
 		return newLine;
