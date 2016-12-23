0bef7cf3a23e3b1c014a6334df2dca72afccb9a9
squirlemaster42 <squirlemaster@gmail.com>
2016-12-23 14:01:33 -0500

    JavaDocs

--- src/org/usfirst/frc/team1699/utils/autonomous/ParserException.java
+++ src/org/usfirst/frc/team1699/utils/autonomous/ParserException.java
@@ -1,3 +1,8 @@
+/**
+ * Exception thrown when parser finds an error
+ * 
+ * @author squirlemaster42
+ */
 package org.usfirst.frc.team1699.utils.autonomous;
 
 public class ParserException extends RuntimeException {
