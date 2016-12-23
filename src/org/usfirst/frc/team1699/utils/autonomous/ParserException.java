d8fb1ad28ded5dfdb03b75c03b952990f8049c37
squirlemaster42 <squirlemaster@gmail.com>
2016-12-23 13:26:06 -0500

    JavDocs

--- src/org/usfirst/frc/team1699/utils/autonomous/ParserException.java
+++ src/org/usfirst/frc/team1699/utils/autonomous/ParserException.java
@@ -2,8 +2,13 @@ package org.usfirst.frc.team1699.utils.autonomous;
 
 public class ParserException extends RuntimeException {
 
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L; //Version id
 
+	/**
+	 * Constructor
+	 * 
+	 * @param msg
+	 */
 	public ParserException(String msg) {
 		super(msg);
 	}
