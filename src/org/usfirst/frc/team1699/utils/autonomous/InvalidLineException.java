03283da182d08d6ed3ad91772e27e0f164e9c5d9
squirlemaster42 <squirlemaster@gmail.com>
2016-12-23 13:25:29 -0500

    JavaDocs

--- src/org/usfirst/frc/team1699/utils/autonomous/InvalidLineException.java
+++ src/org/usfirst/frc/team1699/utils/autonomous/InvalidLineException.java
@@ -2,15 +2,20 @@ package org.usfirst.frc.team1699.utils.autonomous;
 
 public class InvalidLineException extends RuntimeException{
 
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	 * Constructor
+	 */
 	public InvalidLineException() {
 		super();
 	}
 
+	/**
+	 * Constructor
+	 * 
+	 * @param arg0
+	 */
 	public InvalidLineException(String arg0) {
 		super(arg0);
 	}
