f1d1f22906f9b6f8ebbd855b613687abc4d95c38
squirlemaster42 <squirlemaster@gmail.com>
2016-12-23 13:56:09 -0500

    JavaDocs

--- src/org/usfirst/frc/team1699/utils/autonomous/Variable.java
+++ src/org/usfirst/frc/team1699/utils/autonomous/Variable.java
@@ -1,26 +1,53 @@
 package org.usfirst.frc.team1699.utils.autonomous;
 
+//Will be used to store variables created in auto script
 public class Variable {
-	private Type type;
-	private Value value;
+	private Type type; //Stores type
+	private Value value; //Stores value
 	
+	/**
+	 * Constructor
+	 * 
+	 * @param type
+	 * @param value
+	 */
 	public Variable(Type type, Value value){
 		this.type = type;
 		this.value = value;
 	}
 
+	/**
+	 * Returns type
+	 * 
+	 * @return
+	 */
 	public Type getType() {
 		return type;
 	}
 
+	/**
+	 * Sets type
+	 * 
+	 * @param type
+	 */
 	public void setType(Type type) {
 		this.type = type;
 	}
 
+	/**
+	 * Returns value
+	 * 
+	 * @return
+	 */
 	public Value getValue() {
 		return value;
 	}
 
+	/**
+	 * Gets value
+	 * 
+	 * @param value
+	 */
 	public void setValue(Value value) {
 		this.value = value;
 	}
