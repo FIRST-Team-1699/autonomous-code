3affc3429fb1bf1e6219b235d76335952db4e6ab
squirlemaster42 <squirlemaster@gmail.com>
2016-12-23 22:03:04 -0500

    Var utils

--- src/org/usfirst/frc/team1699/utils/autonomous/VariableUtils.java
+++ src/org/usfirst/frc/team1699/utils/autonomous/VariableUtils.java
@@ -5,6 +5,30 @@ import org.usfirst.frc.team1699.utils.autonomous.Tokenizer.Token;
 public class VariableUtils {
 	
 	public static Variable makeVar(String line, Tokenizer tokenizer){
+		tokenizer.tokenize(line);
+		Token tok =  tokenizer.getTokens().get(0);
+		switch(tok.token){
+			case 6: return makeInt(line);
+			case 7: return makeBoolean(line);
+			case 8: return makeDouble(line);
+			case 9: return makeString(line);
+			default: throw new InvalidLineException();
+		}
+	}
+	
+	private static Variable makeInt(String line){
+		return null;
+	}
+
+	private static Variable makeBoolean(String line){
+		return null;
+	}
+	
+	private static Variable makeDouble(String line){
+		return null;
+	}
+	
+	private static Variable makeString(String line){
 		return null;
 	}
 	
