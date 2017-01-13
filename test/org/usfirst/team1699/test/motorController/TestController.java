cbc6d3fbadfac32629635131b8d63607778605c0
squirlemaster42 <squirlemaster@gmail.com>
2017-01-13 17:00:13 -0500

    JavaDocs and other comments

--- test/org/usfirst/team1699/test/motorController/TestController.java
+++ test/org/usfirst/team1699/test/motorController/TestController.java
@@ -1,3 +1,8 @@
+/**
+ * This is a class that is used when a test motor controller is needed
+ * 
+ * @author squirlemaster42
+ */
 package org.usfirst.team1699.test.motorController;
 
 import edu.wpi.first.wpilibj.SpeedController;
@@ -6,21 +11,39 @@ public class TestController implements SpeedController{
 	private boolean inverted;
 	private double speed;
 
+	/**
+	 * Constructor
+	 */
 	public TestController(){
 		this.inverted = false;
 		this.speed = 0;
 	}
 	
+	/**
+	 * Unused
+	 * 
+	 * @param output
+	 */
 	@Override
 	public void pidWrite(double output) {
-		
+		//Unused currently
 	}
 
+	/**
+	 * Returns the speed
+	 * 
+	 * @return speed
+	 */
 	@Override
 	public double get() {
-		return speed;
+		return speed; //Returns the current speed
 	}
 
+	/**
+	 * Sets the speed
+	 * 
+	 * @param speed
+	 */
 	@Override
 	public void set(double speed) {
 		if(inverted){
@@ -30,26 +53,47 @@ public class TestController implements SpeedController{
 		}
 	}
 
+	/**
+	 * Sets if the motor is inverted
+	 * 
+	 * @param isInverted
+	 */
 	@Override
 	public void setInverted(boolean isInverted) {
 		inverted = isInverted;
 	}
 
+	/**
+	 * Returns if the motor is inverted
+	 * 
+	 * @return inverted
+	 */
 	@Override
 	public boolean getInverted() {
 		return inverted;
 	}
 
+	/**
+	 * Sets the speed to zero
+	 */
 	@Override
 	public void disable() {
 		set(0);
 	}
 
+	/**
+	 * Sets the speed of the motor to zero
+	 */
 	@Override
 	public void stopMotor() {
 		set(0);
 	}
 	
+	/**
+	 * Returns the speed
+	 * 
+	 * @return speed
+	 */
 	public double getSpeed(){
 		return this.speed;
 	}
